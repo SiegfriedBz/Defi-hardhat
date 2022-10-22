e97579a8035566b81ab305e7cf00a84506eae30e
siegfried bozza <siegfried.bozza@protonmail.com>
2022-10-22 10:03:26 +0200

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,6 +13,7 @@ module.exports = {
   solidity: {
     compilers: [
       { version: "0.4.19" },
+      { version: "0.6.0" },
       { version: "0.6.12" },
       { version: "0.6.6" },
       { version: "0.8.17" },
